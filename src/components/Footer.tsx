5b5daac3b338dd57579dc3dbcf515f8a495cd726
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-11-01 03:26:57 +0400

    add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+// components/Header.tsx
+
+import React from "react";
+import { styled } from "styled-components";
+import { Container, Typography } from "@mui/material";
+
+const FooterContainer = styled.footer(({ theme }) => ({
+    position: "sticky",
+    bottom: 0,
+    backgroundColor: theme.palette.primary.dark,
+    zIndex: theme.custom.order.footer,
+    padding: `${theme.spacing(4)} 0`,
+    color: theme.palette.text.secondary,
+}));
+
+const FooterContent = styled(Container)({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+});
+
+const Footer: React.FC = () => {
+    return (
+        <FooterContainer>
+            <FooterContent maxWidth="xl">
+                <Typography variant="h4" component="p">
+                    Some Company 2024
+                </Typography>
+            </FooterContent>
+        </FooterContainer>
+    );
+};
+
+export default Footer;
