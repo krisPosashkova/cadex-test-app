7dc03e98358e37543ca8ce010499955db4031c39
Kristina Posashkova <106886437+krisPosashkova@users.noreply.github.com>
2024-11-01 14:07:39 +0400

    update interface

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,9 +1,25 @@
 import { ButtonProps } from "@mui/material";
 
-export interface ICustomButton extends ButtonProps {
+export interface ICustomButtonProps extends ButtonProps {
     href?: string;
     onClick?: () => void;
     bgColor?: string;
     textColor?: string;
     children: React.ReactNode;
+}
+
+export interface IFormInputs {
+    name: string;
+    email: string;
+    message: string;
+}
+
+export interface IFormFieldProps {
+    name: keyof IFormInputs;
+    label: string;
+    control: any;
+    errors: any;
+    multiline?: boolean;
+    rows?: number;
+    rules: object;
 }
